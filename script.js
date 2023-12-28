6cffde093c87b0874b97f16252acf0e6e6ec7622
muh-osman <mr.muhammed.osman@gmail.com>
2023-12-28 23:48:03 +0300

    Fix

--- script.js
+++ script.js
@@ -1,80 +1,58 @@
-let swiperOrderList = [...document.querySelectorAll(".swiper")];
-
-let swiperClassList = [
+const swiperOrderList = Array.from(document.querySelectorAll(".swiper"));
+const swiperClassList = [
   "swiper-1",
   "swiper-2",
   "swiper-3",
   "swiper-4",
   "swiper-5",
 ];
-
-// Set initial active item
 let activeIndex = 2;
-swiperOrderList[activeIndex].classList.add("active");
 
 setInitialClasses();
 
 function setInitialClasses() {
-  swiperOrderList.forEach(function (swiper, i) {
-    swiper.classList.remove(...swiperClassList);
-    swiper.classList.add(swiperClassList[i]);
+  swiperOrderList.forEach((el, i) => {
+    el.classList.remove(...swiperClassList);
+    el.classList.add(swiperClassList[i]);
   });
 }
 
-function setActiveSlide(swiperElement) {
-  swiperOrderList.forEach(function (swiper) {
-    swiper.classList.remove("active");
-  });
-  swiperElement.classList.add("active");
+function setActiveSlide(index) {
+  const rotationAmount =
+    (index - activeIndex + swiperOrderList.length) % swiperOrderList.length;
+  rotateArray(swiperOrderList, rotationAmount);
+  setInitialClasses();
+  activeIndex = index;
 }
-
-// Handle mouse click
-swiperOrderList.forEach(function (swiper, i) {
-  swiper.addEventListener("click", function () {
-    setActiveSlide(swiper);
-
-    const prevActiveIndex = activeIndex;
-    activeIndex = (i + swiperOrderList.length) % swiperOrderList.length;
-
-    const rotationAmount =
-      (activeIndex - prevActiveIndex + swiperOrderList.length) %
-      swiperOrderList.length;
-    rotateArray(swiperOrderList, rotationAmount);
-    setInitialClasses();
-  });
-});
 
 function rotateArray(arr, amount) {
   if (amount > 0) {
-    // Rotate array right
     const removedItems = arr.splice(0, amount);
     arr.push(...removedItems);
   } else if (amount < 0) {
-    // Rotate array left
     const removedItems = arr.splice(amount);
     arr.unshift(...removedItems);
   }
 }
 
-// Handle keyboard arrow keys
+swiperOrderList.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    setActiveSlide(index);
+  });
+});
+
 document.onkeydown = checkKey;
 
 function checkKey(e) {
   e = e || window.event;
 
-  if (e.keyCode == 38) {
-    // Arrow up
+  if (e.keyCode === 38) {
     e.preventDefault();
-    let s = swiperOrderList.pop();
-    swiperOrderList.unshift(s);
-    setInitialClasses();
-    setActiveSlide(swiperOrderList[2]);
-  } else if (e.keyCode == 40) {
-    // Arrow down
+    setActiveSlide(
+      (activeIndex - 1 + swiperOrderList.length) % swiperOrderList.length
+    );
+  } else if (e.keyCode === 40) {
     e.preventDefault();
-    let s = swiperOrderList.shift();
-    swiperOrderList.push(s);
-    setInitialClasses();
-    setActiveSlide(swiperOrderList[2]);
+    setActiveSlide((activeIndex + 1) % swiperOrderList.length);
   }
 }
